cda81c5ab2a1646d0d58f0f38d413e2cc3e89b69
jerrydzhang <jerry.zhang.d@gmail.com>
2025-10-06 20:25:59 -0400

    fixed flake for hpc

--- flake.nix
+++ flake.nix
@@ -5,39 +5,58 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    inherit (nixpkgs) lib;
-    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-  in {
-    devShells = forAllSystems (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            (python3.withPackages (ps:
-              with ps; [
-                ruff
-                pytest
-                mypy
-              ]))
-            pyright
-            uv
-          ];
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              (python3.withPackages (
+                ps: with ps; [
+                  ruff
+                  pytest
+                  mypy
+                ]
+              ))
+              pyright
+              uv
+            ];
 
-          LD_LIBRARY_PATH = [
-            "$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib"
-          ];
+            LD_LIBRARY_PATH = [
+              "$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib"
+            ];
 
-          shellHook = ''
-            unset PYTHONPATH
-						uv sync --inexact
-						uv pip install -e . -e "jernerics @ ./../jernerics"
-            . .venv/bin/activate
-          '';
-        };
-      }
-    );
-  };
+            shellHook = ''
+              unset PYTHONPATH
+              uv sync --inexact
+              uv pip install -e . -e "jernerics @ ./../jernerics"
+              . .venv/bin/activate
+            '';
+          };
+
+          hpc = pkgs.mkShell {
+            packages = with pkgs; [
+              python3
+              uv
+            ];
+
+            shellHook = ''
+              unset PYTHONPATH
+              uv sync --inexact
+              uv pip install -e . -e "jernerics @ ./../jernerics"
+              . .venv/bin/activate
+            '';
+          };
+        }
+      );
+    };
 }
-
